9c4980c4883e97fe1b227d68843fe109361bd617
Ran.Fang <ranfang66@gmail.com>
2020-06-04 23:53:37 +0800

    add some fileIO practice files

---
+++ ch3_fileIO/pra3_4.c
@@ -0,0 +1,48 @@
+#include "../apue.h"
+#include <fcntl.h>
+
+int main(int argc, const char *argv[]){
+    int val;
+
+    if (argc != 2){
+        err_quit("usage: please add the <descriptor#> as the argument");
+    }
+
+    if ((val = fcntl(atoi(argv[1]), F_GETFL, 0)) < 0){
+        err_sys("fcntl error for fd %d", atoi(argv[1]));
+    }
+
+    switch (val & O_ACCMODE) {
+        case O_RDONLY:
+            printf("read only");
+            break;
+
+        case O_WRONLY:
+            printf("write only");
+            break;
+
+        case O_RDWR:
+            printf("read write");
+            break;
+
+        default:
+            err_dump("unknow access mode");
+    }
+
+    if (val & O_APPEND)
+        printf(", append");
+
+    if(val & O_NONBLOCK)
+        printf(", nonblocking");
+
+    if(val & O_SYNC)
+        printf(", synchronous writes");
+
+#if !defined(_POSIX_C_SOURCE) && defined(O_FSYNC) && (O_SYNC != O_FSYNC)
+    if (val & O_FSYNC)
+        printf(", synchronous writes");
+#endif
+
+    putchar('\n');
+    exit(0);
+}
